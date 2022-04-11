612be4f9e1de71a2d088ee7df05aa2d50b59ed8c
DESKTOP-6U832JM\Lucas Chagas <lucasandrade595@gmail.com>
2022-04-11 14:11:34 -0300

    Feat: Conectando com leitor EDGE 50 usando DLL disponibilizada pela Acura para realizar leitura de tags RFID.

---
+++ TESTE_RFID/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ThingMagic;
+
+namespace TESTE_RFID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SDKUHFReader MyReader;
+
+            MyReader = new SDKUHFReader(@"C:\Apps\Config\ACU100.UHF_cnx", @"C:\Apps\Config\ACU100.UHF_cfg", false, false);
+            MyReader.TagDataType = UHFTagReturnType.EPC;
+            MyReader.SetMode(UHFReaderMode.READING);
+            MyReader.OnDeviceException += MyReader_OnDeviceException;
+            MyReader.OnAgentException += MyReader_OnAgentException;
+            MyReader.OnReadEPC += MyReader_OnReadEPC;
+            MyReader.OnReadingStatus += MyReader_OnReadingStatus;
+            MyReader.OnTagEnter += MyReader_OnTagEnter;
+            MyReader.OnTagLeave += MyReader_OnTagLeave;
+            MyReader.OnConnectionParam += MyReader_OnConnectionParam;
+            MyReader.OnNoNewTag += MyReader_OnNoNewTag;
+            MyReader.OnNoTag += MyReader_OnNoTag;
+            MyReader.OnPing += MyReader_OnPing;
+            Connect();
+        }
+    }
+}
---
+++ TESTE_RFID_FRAMEWORK/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using ThingMagic;
+
+namespace TESTE_RFID_FRAMEWORK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
+        {
+            try
+            {
+                int timeout = 2000;
+
+                //CONEXAO
+                using (Reader reader = Reader.Create("eapi:///com5"))
+                {
+                    reader.Connect();
+
+                    if ((reader is SerialReader) || (reader is LlrpReader))
+                    {
+                        if (Reader.Region.UNSPEC == (Reader.Region)reader.ParamGet("/reader/region/id"))
+                        {
+                            Reader.Region[] supportedRegions = (Reader.Region[])reader.ParamGet("/reader/region/supportedRegions");
+                            if (supportedRegions.Length < 1)
+                            {
+                                throw new FAULT_INVALID_REGION_Exception();
+                            }
+                            else
+                            {
+                                reader.ParamSet("/reader/region/id", supportedRegions[0]);
+                            }
+                        }
+                    }
+
+                    //ANTENAS
+                    int[] antennaList = new int[1];
+                    antennaList[0] = 2;
+
+                    //string str = "1,1";
+                    //antennaList = Array.ConvertAll<string, int>(str.Split(','), int.Parse);
+                    SimpleReadPlan plan = new SimpleReadPlan(antennaList, TagProtocol.GEN2, null, null, 1000);
+                    reader.ParamSet("/reader/read/plan", plan);
+
+                    //LEITURA
+                    reader.TagRead += TagRead;
+                    TagReadData[] tags = new TagReadData[0];
+
+                    //POTENCIA DA LEITURA
+                    int readPowerMax = (int)reader.ParamGet("/reader/radio/powerMax");
+                    int readPowerMin = (int)reader.ParamGet("/reader/radio/powerMin");
+                    reader.ParamSet("/reader/radio/readPower", 2150);
+
+                    Console.WriteLine($"Read power Max: {readPowerMax}");
+                    Console.WriteLine($"Read power Min: {readPowerMin}");
+
+                    //LE TODAS AS TAGS DURANTE O PERIODO DEFINIDO E DEPOIS FAZ A SUA LEITURA EM FORMA DE LISTA OU ARRAY
+                    Console.WriteLine($"{DateTime.Now}: Inicio a leitura");
+                    tags = reader.Read(10000);
+
+                    using (StreamWriter file = File.CreateText(@"C:/AcuraRfid/teste.txt"))
+                    {
+                        foreach (var item in tags)
+                        {
+                            file.WriteLine(item.Tag + "\n");
+                            Console.WriteLine($"{DateTime.Now}: {item.Tag}");
+                        }
+
+                        reader.StopReading();
+
+                        Console.WriteLine($"{DateTime.Now}: Parou a leitura");
+
+                        //Console.WriteLine($"{DateTime.Now}: Inicio a leitura");
+                        //reader.Read(timeout);
+                        //reader.StartReading();
+
+                        //await Task.Delay(100000);
+
+                        //foreach (var item in tags)
+                        //{
+                        //    file.WriteLine(item.Tag + "\n");
+                        //    Console.WriteLine($"{DateTime.Now}: {item.Tag}");
+                        //}
+                        Console.WriteLine($"{DateTime.Now}: Terminou a leitura");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.ReadKey();
+        }
+
+        //FUNCIONA SOMENTE SE O STARTREADING FOR ATIVADO
+        private static void TagRead(object sender, TagReadDataEventArgs e)
+        {
+            Console.WriteLine($"vvv {DateTime.Now}: {e.TagReadData.Tag}");
+        }
+
+        //AO PARAR E INICIAR A LEITURA ELE LE NOVAMENTE A TAG, NÃO GUARDA CACHE
+    }
+}
+
